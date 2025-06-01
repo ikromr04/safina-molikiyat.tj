4808d3940e0c53e45b8bae66afbae89d58da9fb5
ikromr04 <ikromr04@gmail.com>
2025-06-01 21:04:05 +0500

    Transform section make

---
+++ resources/views/components/blocks/transform.blade.php
@@ -0,0 +1,37 @@
+<section class="container">
+  <h2 class="title mt-0 mb-6 md:mb-8 xl:mb-6 2xl:mb-7">
+    <small class="block">Превратите ваш объект</small> в бизнес-центр
+  </h2>
+
+  <div class="pl-[10px] md:pl-[calc(10px+4*((100vw-768px)/512))] xl:pl-[calc(14px+2*((100vw-1280px)/(1536-1280)))] 2xl:pl-[15px]">
+    <strong class="block font-normal mb-4 md:leading-none md:text-[calc(16px+8*((100vw-768px)/512))] md:mb-[calc(16px+24*((100vw-768px)/512))] xl:text-[min(calc(24px+6*((100vw-1280px)/256)),30px)] xl:mb-[min(calc(40px+8*((100vw-1280px)/256)),48px)]">
+      Доверьте управление недвижимостью профессионалам
+    </strong>
+
+    <ul class="font-light list-none p-0 m-0 flex flex-col gap-4 mb-6 text-sm md:grid md:grid-cols-2 md:gap-x-[70px] md:mb-[calc(24px+24*((100vw-768px)/512))] md:text-[calc(14px+4*((100vw-768px)/512))] md:px-[calc(100*((100vw-768px)/512))] xl:text-[min(calc(18px+2*((100vw-1280px)/256)),20px)] xl:gap-x-[min(calc(70px+40*((100vw-1280px)/256)),110px)] xl:px-[min(calc(100px+16*((100vw-1280px)/256)),116px)]">
+      <li>
+        Наша команда – это международные юристы и аналитики, которые разрабатывают оптимальные стратегии управления для вашего объекта, чтобы обеспечить стабильный доход и высокую ликвидность.
+      </li>
+      <li>
+        Мы гордимся своей приверженностью высоким стандартам и используем передовые инженерные и технические решения для создания комфортных и эффективных бизнес-пространств.
+      </li>
+    </ul>
+
+    <strong class="block font-normal mb-4 md:leading-none md:text-[calc(16px+8*((100vw-768px)/512))] md:mb-[calc(16px+24*((100vw-768px)/512))] xl:text-[min(calc(24px+6*((100vw-1280px)/256)),30px)] xl:mb-[min(calc(40px+8*((100vw-1280px)/256)),48px)]">
+      Гарантия качества и соблюдения всех обязательств
+    </strong>
+
+    <ol class="font-light pl-5 m-0 flex flex-col gap-4 mb-6 text-sm md:grid md:grid-cols-3 md:gap-x-12 md:mb-[calc(24px+24*((100vw-768px)/512))] md:text-[calc(14px+4*((100vw-768px)/512))] md:px-[calc(16px+100*((100vw-768px)/512))] xl:text-[min(calc(18px+2*((100vw-1280px)/256)),20px)] xl:gap-x-[min(calc(48px+62*((100vw-1280px)/256)),110px)] xl:px-[min(calc(100px+16*((100vw-1280px)/256)),116px)]">
+      <li>
+        Комплексное управление объектом и взаимоотношений со стейкхолдерами*.
+        <span class="block mt-4 text-[10px] xl:text-[calc(10px+2*((100vw-1280px)/256))]">*от подрядных услуг до поддержки функциональности объекта</span>
+      </li>
+      <li>
+        Экономия затрат благодаря оптимизации эксплуатационных расходов.
+      </li>
+      <li>
+        Профессиональная юридическая поддержка и финансовый анализ и управленческие рекомендации
+      </li>
+    </ol>
+  </div>
+</section>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
   <title>{{ env('APP_NAME') }}</title>
 </head>
 
-<body class="bg-onyx text-platinum m-0 group leading-[1.2] @md('bg-red-500')">
+<body class="bg-onyx text-platinum m-0 group leading-[1.2]">
   @include('layouts.icons')
 
   @include('layouts.header')
